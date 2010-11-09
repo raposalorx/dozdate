4b66d29b1d88403986270750f3ba493d8660dfca
Brian lorx Robertson <raposa.msscripting@gmail.com>
2010-11-08 23:25:39 -0700

    Updated so that it works on 32-bit machines.
    Syntax cleanup is coming soon.

--- dozdate.hs
+++ dozdate.hs
@@ -1,16 +1,16 @@
 import System( getArgs )
 
-translateBase :: Int -> String
-translateBase 10 = "X"
-translateBase 11 = "E"
+translateBase :: Integer -> String
+translateBase 10 = "χ"
+translateBase 11 = "Ɛ"
 translateBase x = show x
 
 main = do
 	args <- getArgs
-	let hour = read $ args!!0 :: Int
-	let min = read $ args!!1 :: Int
-	let sec = read $ args!!2 :: Int
-	let mil = (read $ args!!3 :: Int) `div` 1000000
+	let hour = read $ args!!0 :: Integer
+	let min = read $ args!!1 :: Integer
+	let sec = read $ args!!2 :: Integer
+	let mil = (read $ args!!3 :: Integer) `div` 1000000
 	let time1 = (*) 144 . (+) mil . (*) 1000 . (+) sec $ (+) ((*) hour 3600) ((*) min 60)
 	let time2 = (/) (fromIntegral time1) 25
 	let time = floor $ (/) time2 1000
@@ -24,6 +24,7 @@ main = do
 	let sec2 = translateBase $ mod dozSec 12
 	let dozSec1 = translateBase $ floor $ (/) (fromIntegral sec) 10
 	let dozSec2 = translateBase $ mod sec 10
+	
 	putStrLn $	(if hour < 12 
 				then "0" ++ (dozHour1) else "1" ++ (dozHour2))
 				++ ":" ++ (dozMin1) ++ (dozMin2) ++ ":" ++ (sec1) ++ (sec2)
